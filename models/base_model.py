c004b393a10a6c4dc089dd7d681b90d985662fa8
el hihi zakarya <zakarya.elhihi@gmail.com>
2023-11-12 05:21:31 -0800

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 # BaseModel defines all common attributes/methods for other classes
 import uuid
 from datetime import datetime
-
+from models import storage
 
 class BaseModel:
     """ base model """
@@ -11,6 +11,8 @@ class BaseModel:
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
         else:
             dateformat = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
@@ -32,6 +34,7 @@ class BaseModel:
     def save(self):
         """ save methode """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ to dict methode """
