44d7cc13fa85b908a0830771d6137e47b960e5a5
el hihi zakarya <zakarya.elhihi@gmail.com>
2023-11-12 04:08:05 -0800

    setting up

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ from datetime import datetime
 class BaseModel:
     """ base model """
     def __init__(self, *args, **kwargs):
+        """ init methode """
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -19,6 +20,7 @@ class BaseModel:
                     setattr(self, key, value)
 
     def __str__(self):
+        """ str methode """
         temp_dict = {}
         for key, value in self.__dict__.items():
             if (not value) is False:
@@ -28,11 +30,11 @@ class BaseModel:
         return B + " (" + self.id + ")" + A
 
     def save(self):
-
+        """ save methode """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-
+        """ to dict methode """
         temp_dict2 = {}
         dateformat = "%Y-%m-%dT%H:%M:%S.%f"
         for k, v in self.__dict__.items():
