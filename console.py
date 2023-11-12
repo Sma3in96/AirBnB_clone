8e77d3eb4689cd8a3bed53792060dfbdd67f3175
el hihi zakarya <zakarya.elhihi@gmail.com>
2023-11-12 15:20:37 -0800

    console.py

--- console.py
+++ console.py
@@ -2,8 +2,13 @@
 """ creating cmd interpreter """
 import cmd
 import sys
-"""from models.base_model import BaseModel
-import json"""
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+import json
 
 
 class HBNBCommand(cmd.Cmd):
