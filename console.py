3d125a715f31995fd5738bff81cb3fcad8605600
Sma3in96 <oumouss.ismail5@gmail.com>
2023-11-13 00:47:01 +0100

    task8

--- console.py
+++ console.py
@@ -2,17 +2,24 @@
 """ creating cmd interpreter """
 import cmd
 import sys
+from shlex import split
 from models.base_model import BaseModel
+from models.user import User
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-import json
+from models import storage
+"""import json"""
 
 
 class HBNBCommand(cmd.Cmd):
     """class of hbnb prompt """
+
+    __dict_class = {'BaseModel': BaseModel, 'User': User, 'Amenity': Amenity,
+                    'City': City, 'Place': Place,
+                    'Review': Review, 'State': State}
 
     def help_help(self, line):
         """helps u on help"""
@@ -30,6 +37,41 @@ class HBNBCommand(cmd.Cmd):
         """end of file"""
         print()
         return True
+
+    def do_show(self, line):
+        """show methode"""
+        arg = line.split()
+        objec_dict = storage.all()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in HBNBCommand.__dict_class.key():
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(arg[0], arg[1]) not in objec_dict:
+            print("** no instance found **")
+        else:
+            print(objec_dict["{}.{}".format(arg[0], arg[1])])
+
+    def do_count(self, cls_name):
+        """count"""
+        count = 0
+        for obj in storage.all().values():
+            if obj.__class__.__name__ == cls_name:
+                count = count + 1
+        print(count)
+
+    def do_create(self, classins):
+        """ Creates an instance according to a given class """
+
+        if not classins:
+            print("** class name missing **")
+        elif classins not in HBNBCommand.__dict_class.keys():
+            print("** class doesn't exist **")
+        else:
+            my_model = HBNBCommand.__dict_class[classins]()
+            print(my_model.id)
+            my_model.save()
 
 
 if __name__ == '__main__':
