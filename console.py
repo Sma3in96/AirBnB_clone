bcb1b2911fd158a1d027f68dc7613b06c9e4bcef
Sma3in96 <oumouss.ismail5@gmail.com>
2023-11-12 15:16:24 +0100

    nothing working

--- console.py
+++ console.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
 """ creating cmd interpreter """
 import cmd
-from models.base_model import BaseModel
-import json
+"""from models.base_model import BaseModel
+import json"""
 
 
 class HBNBCommand(cmd.Cmd):
     """class of hbnb prompt """
+
+    def help_help(self):
+        """helps u on help"""
+        print("Provides description of a given command")
+
+    def emptyline(self):
+        """ empty line entered"""
+        pass
+
+    def do_quit(self):
+        """quit the console"""
+        return True
+
+    def do_EOF(self):
+        """end of file"""
+        print()
+        return True
 
 
 if __name__ == '__main__':
