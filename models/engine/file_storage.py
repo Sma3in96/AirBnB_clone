70ecd0e0217dc136e563015db01d04c9f3c64d4b
Sma3in96 <oumouss.ismail5@gmail.com>
2023-12-05 01:33:04 +0100

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,16 +2,28 @@
 """ file storage class creating """
 import json
 import os
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.city import City
 
 
 class FileStorage:
     """class of FileStorage"""
     __file_path = "store.json"
     __objects = {}
+    dct_class = {'BaseModel': BaseModel,
+                 'User': User,
+                 'Place': Place,
+                 'City': City, 'Amenity': Amenity, 'State': State,
+                 'Review': Review}
 
     def new(self, obj):
         """methode that create a new obj"""
-        k = obj.__class__.__name__ + "." + obj.id
+        k = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[k] = obj
 
     def all(self):
@@ -29,14 +41,9 @@ class FileStorage:
 
     def reload(self):
         """ reload a object"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.amenity import Amenity
-        from models.state import State
-        from models.review import Review
-        from models.place import Place
-        from models.city import City
-        dct_class = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        dct_class = {'BaseModel': BaseModel,
+                     'User': User,
+                     'Place': Place,
                      'City': City, 'Amenity': Amenity, 'State': State,
                      'Review': Review}
         file_path = FileStorage.__file_path
@@ -48,3 +55,23 @@ class FileStorage:
                     class_name = value['__class__']
                     instance = dct_class[class_name](**value)
                     self.new(instance)
+
+    def count(self, class_name):
+        """ count methode """
+        count = 0
+        for key in self.__objects:
+            if class_name == key.split('.')[0]:
+                count += 1
+        return count
+
+    def all_by_class(self, cls):
+        """ return a dict """
+        class_objects = {}
+        for key, value in self.__objects.items():
+            if type(value).__name__ == cls:
+                class_objects[key] = value
+        return class_objects
+
+    @staticmethod
+    def get_classes():
+        return FileStorage.dct_class
